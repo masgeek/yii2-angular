2a5ff732a643ccead75808a1a01f46bc4d64e1b8
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-22 13:05:30 +0300

    Bower init file

---
+++ frontend/assets/AngularAsset.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: barsa
+ * Date: 3/22/2017
+ * Time: 12:58 PM
+ */
+
+namespace frontend\assets;
+
+
+use yii\web\AssetBundle;
+use yii\web\View;
+
+class AngularAsset extends AssetBundle
+{
+    public $sourcePath = '@bower';
+    public $js = [
+        'angular/angular.js',
+        'angular-route/angular-route.js',
+        'angular-strap/dist/angular-strap.js',
+    ];
+    public $jsOptions = [
+        'position' => View::POS_HEAD,
+    ];
+}
