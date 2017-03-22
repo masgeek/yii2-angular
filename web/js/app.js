a52ec8e9dd8b8862235c54a99c978efc8a5be8fe
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-22 12:52:08 +0300

    Bower init file

--- web/js/app.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by barsa on 3/22/2017.
- */
-var app = angular.module('app', [
-    'ngRoute',      //$routeProvider
-    'mgcrea.ngStrap'//bs-navbar, data-match-route directives
-]);
